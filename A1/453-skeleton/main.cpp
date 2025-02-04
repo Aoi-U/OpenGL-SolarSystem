b0b0f938a48e7d6e4ae28f429aaaa6e277f5abd5
Aoi-U <aoilovepokemon@gmail.com>
2025-02-04 15:37:09 -0700

    working imgui

--- A1/453-skeleton/main.cpp
+++ A1/453-skeleton/main.cpp
@@ -13,34 +13,11 @@
 #include "AssetPath.h"
 
 #include "imgui.h"
-//#include "imgui_impl_glfw.h"
-//#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-const double PI = 3.14159265358979323846;
 
-// EXAMPLE CALLBACKS
-
-class MyCallbacks : public CallbackInterface {
-
-public:
-	MyCallbacks(ShaderProgram& shader) : shader(shader) {}
-
-	virtual void keyCallback(int key, int scancode, int action, int mods) {
-		if (key == GLFW_KEY_R && action == GLFW_PRESS) { // On a positive edge press (when FIRST clicked)
-			shader.recompile();
-		}
-	}
-
-// 	// Other callbacks we implemented that you may use
-// 	//virtual void mouseButtonCallback(int button, int action, int mods) {}
-// 	//virtual void cursorPosCallback(double xpos, double ypos) {}
-// 	//virtual void scrollCallback(double xoffset, double yoffset) {}
-// 	//virtual void windowSizeCallback(int width, int height) { CallbackInterface::windowSizeCallback(width, height);/*Should be called*/ }
-
-private:
-	ShaderProgram& shader;
-};
-
+// Callback class incase ImGui fails
 class KeyPresses : public CallbackInterface {
 
 public:
@@ -74,7 +51,6 @@ private:
 	int& shape;
 	int& max_iterations;
 };
-// END EXAMPLES
 
 // defines a point with x and y values
 struct cord {
@@ -133,7 +109,7 @@ std::vector<triangle> calculate_triangle(cord left, double length, double iterat
 			{
 				left, // bottom left corner
 				{left.x + length, left.y}, // bottom right corner 
-				{left.x + length / 2, left.y + length * sin(PI / 3)} // top corner
+				{left.x + length / 2, left.y + length * sin(glm::pi<double>() / 3)} // top corner
 			}
 		};
 	}
@@ -142,21 +118,21 @@ std::vector<triangle> calculate_triangle(cord left, double length, double iterat
 	triangle triangle_left {
 		left,
 		{left.x + length / 2, left.y},
-		{left.x + length / 4, left.y + length / 2 * sin(PI / 3)}
+		{left.x + length / 4, left.y + length / 2 * sin(glm::pi<double>() / 3)}
 	};
 
 	// calculates points for the right subtriangle
 	triangle triangle_right {
 		triangle_left.right,
 		{triangle_left.right.x + length, triangle_left.right.y},
-		{triangle_left.right.x + length / 2, triangle_left.right.y + length * sin(PI / 3)}
+		{triangle_left.right.x + length / 2, triangle_left.right.y + length * sin(glm::pi<double>() / 3)}
 	};
 
 	// calculates points for the top subtriangle
 	triangle triangle_top {
 		triangle_left.top,
 		triangle_right.top,
-		{triangle_left.top.x + length / 2, triangle_left.top.y + length * sin(PI / 3)}
+		{triangle_left.top.x + length / 2, triangle_left.top.y + length * sin(glm::pi<double>() / 3)}
 	};
 
 	iteration--;
@@ -196,8 +172,8 @@ std::vector<levy> calculate_levy(cord start, double length, double alpha, double
 	levy first {
 		start,
 		{
-			start.x + length * cos(alpha + PI / 4),
-			start.y + length * sin(alpha + PI / 4)
+			start.x + length * cos(alpha + glm::pi<double>() / 4),
+			start.y + length * sin(alpha + glm::pi<double>() / 4)
 		}
 	};
 
@@ -205,16 +181,16 @@ std::vector<levy> calculate_levy(cord start, double length, double alpha, double
 	levy second {
 		first.end,
 		{
-			first.end.x + length * cos(alpha + PI / 4),
-			first.end.y + length * sin(alpha + PI / 4)
+			first.end.x + length * cos(alpha + glm::pi<double>() / 4),
+			first.end.y + length * sin(alpha + glm::pi<double>() / 4)
 		}
 	};
 	
 	iteration--;
 
 	// recursively call calculate_levy for each subline
-	std::vector<levy> result_first = calculate_levy(first.start, length, alpha + PI / 4, iteration);
-	std::vector<levy> result_second = calculate_levy(second.start, length, alpha - PI / 4, iteration);
+	std::vector<levy> result_first = calculate_levy(first.start, length, alpha + glm::pi<double>() / 4, iteration);
+	std::vector<levy> result_second = calculate_levy(second.start, length, alpha - glm::pi<double>() / 4, iteration);
 
 	// combine all line segments into a single vector and return
 	std::vector<levy> result {};
@@ -265,8 +241,8 @@ std::vector<tree> calculate_tree(cord start, double length, double alpha, double
 	tree left_branch {
 		mid, 
 		{
-			mid.x + length * cos(alpha + 25.7 * (PI/180)),
-			mid.y + length * sin(alpha + 25.7 * (PI/180))
+			mid.x + length * cos(alpha + 25.7 * (glm::pi<double>() / 180)),
+			mid.y + length * sin(alpha + 25.7 * (glm::pi<double>() / 180))
 		},
 		col
 	};
@@ -274,8 +250,8 @@ std::vector<tree> calculate_tree(cord start, double length, double alpha, double
 	tree right_branch {
 		mid,
 		{
-			mid.x + length * cos(alpha - 25.7 * (PI/180)),
-			mid.y + length * sin(alpha - 25.7 * (PI/180))
+			mid.x + length * cos(alpha - 25.7 * (glm::pi<double>() / 180)),
+			mid.y + length * sin(alpha - 25.7 * (glm::pi<double>() / 180))
 		},
 		col
 	};
@@ -291,8 +267,8 @@ std::vector<tree> calculate_tree(cord start, double length, double alpha, double
 
 	iteration--;
 
-	std::vector<tree> result_left = calculate_tree(left_branch.start, length, alpha + 25.7 * (PI/180), iteration, max_iteration);
-	std::vector<tree> result_right = calculate_tree(right_branch.start, length, alpha - 25.7 * (PI/180), iteration, max_iteration);
+	std::vector<tree> result_left = calculate_tree(left_branch.start, length, alpha + 25.7 * (glm::pi<double>() /180), iteration, max_iteration);
+	std::vector<tree> result_right = calculate_tree(right_branch.start, length, alpha - 25.7 * (glm::pi<double>() /180), iteration, max_iteration);
 	std::vector<tree> result_top = calculate_tree(top_branch.start, length, alpha, iteration, max_iteration);
 
 	std::vector<tree> result {branch};
@@ -312,7 +288,7 @@ void render_serpinski(CPU_Geometry& cpuGeom, int iterations) {
 	double current = 0.f; // starting value
 
 	// for each triangle, add its vertices and color to cpuGeom
-	for (triangle triangle : triangles) {
+	for (const triangle& triangle : triangles) {
 		cpuGeom.verts.push_back(glm::vec3(triangle.left.x, triangle.left.y, 0.f));
 		cpuGeom.verts.push_back(glm::vec3(triangle.right.x, triangle.right.y, 0.f));
 		cpuGeom.verts.push_back(glm::vec3(triangle.top.x, triangle.top.y, 0.f));
@@ -326,13 +302,13 @@ void render_serpinski(CPU_Geometry& cpuGeom, int iterations) {
 
 void render_levy(CPU_Geometry& cpuGeom, int iterations) {
 	// call calculate_levy to get all vertices of the levy c curve
-	std::vector<levy> levys = calculate_levy({0.f, -0.4f}, 0.8f, PI / 2, iterations);
+	std::vector<levy> levys = calculate_levy({0.f, -0.4f}, 0.8f, glm::pi<double>() / 2, iterations);
 
 	double step = 1.f / levys.size(); // color increment value for each line segment
 	double current = 0.f; // starting value
 
 	// for each line segment, add its vertices and color to cpuGeom
-	for (levy levy : levys) {
+	for (const levy& levy : levys) {
 		cpuGeom.verts.push_back(glm::vec3(levy.start.x, levy.start.y, 0.f));
 		cpuGeom.verts.push_back(glm::vec3(levy.end.x, levy.end.y, 0.f));
 
@@ -344,9 +320,9 @@ void render_levy(CPU_Geometry& cpuGeom, int iterations) {
 
 void render_tree(CPU_Geometry& cpuGeom, int iterations) {
 	// call calculate_tree to get all vertices of the tree
-	std::vector<tree> trees = calculate_tree({0.f, -0.8f}, 0.8f, PI / 2, iterations, iterations);
+	std::vector<tree> trees = calculate_tree({0.f, -0.8f}, 0.8f, glm::pi<double>() / 2, iterations, iterations);
 
-	for (tree branch : trees) {
+	for (const tree& branch : trees) {
 		cpuGeom.verts.push_back(glm::vec3(branch.start.x, branch.start.y, 0.f));
 		cpuGeom.verts.push_back(glm::vec3(branch.end.x, branch.end.y, 0.f));
 
@@ -364,11 +340,13 @@ int main() {
 
 	GLDebug::enable(); // ON Submission you may comments this out to avoid unnecessary prints to the console
 
+	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
-	//ImGui::CreateContext();
-	//ImGuiIO& io = ImGui::GetIO(); (void)io;
-	//ImGui::StyleColorsDark();
-	//ImGui_ImplGlfw_InitForOpenGL(window.getGLFWwindow(), true);
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
+	ImGui_ImplGlfw_InitForOpenGL(window.getGLFWwindow(), true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
 
 	// SHADERS
 	ShaderProgram shader(
@@ -381,14 +359,17 @@ int main() {
 	int max_iterations = 10;
 	int iterations = 0;
 	int shape = 0;
+	int prevShape = shape;
+
+	std::string shapeName = "Serpinski's Triangle";
 
 	// CALLBACKS
 	// std::shared_ptr<MyCallbacks> callback_ptr = std::make_shared<MyCallbacks>(shader); // Class To capture input events
 	// //std::shared_ptr<MyCallbacks2> callback2_ptr = std::make_shared<MyCallbacks2>();
 	// window.setCallbacks(callback_ptr); // Can also update callbacks to new ones as needed (create more than one instance)
 
-	std::shared_ptr<KeyPresses> callback_ptr = std::make_shared<KeyPresses>(iterations, shape, max_iterations);
-	window.setCallbacks(callback_ptr);
+	//std::shared_ptr<KeyPresses> callback_ptr = std::make_shared<KeyPresses>(iterations, shape, max_iterations);
+	//window.setCallbacks(callback_ptr);
 
 	// GEOMETRY
 	CPU_Geometry cpuGeom; // Just a collection of vectors
@@ -397,26 +378,40 @@ int main() {
 
 	while (!window.shouldClose()) {
 		glfwPollEvents(); // Propagate events to the callback class
+		ImGui_ImplOpenGL3_NewFrame();
 
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		cpuGeom.verts.clear();
+		cpuGeom.cols.clear();
 		int nVerts = 0;
+
+		if (prevShape != shape) {
+			iterations = 0;
+			prevShape = shape;
+		}
 		switch (shape) {
 			case 0:
-				render_serpinski(cpuGeom, iterations);
 				render_mode = GL_TRIANGLES;
 				nVerts = calculate_num_vertices(iterations, shape);
-				max_iterations = 10;
+				max_iterations = 9;
+				shapeName = "Serpinski's Triangle";
+				render_serpinski(cpuGeom, iterations);
 				break;
 			case 1:
-				render_levy(cpuGeom, iterations);
 				render_mode = GL_LINES;
 				nVerts = calculate_num_vertices(iterations, shape);
 				max_iterations = 15;
+				shapeName = "C Levy Curve";
+				render_levy(cpuGeom, iterations);
 				break;
 			case 2:
-				render_tree(cpuGeom, iterations);
 				render_mode = GL_LINES;
 				nVerts = calculate_num_vertices(iterations, shape);
 				max_iterations = 10;
+				shapeName = "Tree";
+				render_tree(cpuGeom, iterations);
 				break;
 		}
 
@@ -431,11 +426,22 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear render screen (all zero) and depth (all max depth)
 		glDrawArrays(render_mode, 0, nVerts); // Render Triangle primatives, starting at index 0 (first) with a total of nVerts
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui (if used)
-		cpuGeom.verts.clear();
-		cpuGeom.cols.clear();
+
+		ImGui::Begin("My name is window, ImGUI window");
+		ImGui::Text("Hello world!");
+		ImGui::SliderInt("Shape", &shape, 0, 2, shapeName.c_str());
+		ImGui::SliderInt("Iteration", &iterations, 0, max_iterations, std::to_string(iterations).c_str());
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		window.swapBuffers(); //Swap the buffers while displaying the previous 
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate(); // Clean up GLFW
 	return 0;
